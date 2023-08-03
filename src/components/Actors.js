fef9c91b934f969a537ff27e26cf1d0873ebec9e
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-08-03 14:20:01 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,36 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+	return (
+		<div>
+			<NavBar />
+
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route exact path="/movies">
+					<Movies />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route exact path="/directors">
+					<Directors />
+				</Route>
+			</Switch>
+			<Switch>
+				<Route exact path="/actors">
+					<Actors />
+				</Route>
+			</Switch>
+		</div>
+	);
+	);
 }
+
+
 
 export default Actors;
